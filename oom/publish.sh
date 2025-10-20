69ddaf67d0537f6cce596ecc75a7c0eca7b7af22
Eddie Webbinaro <5262154+eddiewebb@users.noreply.github.com>
2025-10-20 10:41:49 -0400

    fix publish and image

--- oom/publish.sh
+++ oom/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build --build-arg STRESS_MEM_SIZE=120M . -t ghcr.io/eddiewebb/oom:oom
+docker build --build-arg STRESS_MEM_SIZE=120M ./image -t ghcr.io/eddiewebb/oom:oom
 docker push ghcr.io/eddiewebb/oom:happy
-docker build --build-arg STRESS_MEM_SIZE=50M . -t ghcr.io/eddiewebb/oom:happy
+docker build --build-arg STRESS_MEM_SIZE=50M ./image -t ghcr.io/eddiewebb/oom:happy
 docker push ghcr.io/eddiewebb/oom:oom
