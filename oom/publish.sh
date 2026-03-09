3fee7c22376cdb20e157d090b96996dfc18c4d73
Eddie Webbinaro <5262154+eddiewebb@users.noreply.github.com>
2026-03-10 00:09:19 +0900

    Delete oom/publish.sh

--- oom/publish.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker build --build-arg STRESS_MEM_SIZE=120M ./image -t ghcr.io/eddiewebb/oom:oom
-docker push ghcr.io/eddiewebb/oom:happy
-docker build --build-arg STRESS_MEM_SIZE=50M ./image -t ghcr.io/eddiewebb/oom:happy
-docker push ghcr.io/eddiewebb/oom:oom
